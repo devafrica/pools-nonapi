ad7952976ad6c8691e340ee483e48ec19e9fb3e9
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-28 09:43:22 +0200

    Update list.php

--- list.php
+++ list.php
@@ -1,11 +1,3 @@
-["poolparty.cryptopay.org.za", "https://partystats.cryptopay.org.za"],
-["letshash.it","https://letshash.it:8273"],
-["cpacoin.my-mining-pool.de", "https://apicpacoin.my-mining-pool.de"]
-//["miningcache.com", "https://miningcache.com:8119"],
-//["cpa.cryptocoins-digging.space", "https://cpa.cryptocoins-digging.space:8119"],
-//["cpa.thecoinsmining.com", "https://cpa.thecoinsmining.com:8119"],
-//["cpa.cnhub.de", "http://cpa.cnhub.de:8127"],
-//["cpa.multiminer.us", "https://cpa.multiminer.us:8119"],
-//["cpa.cryptomine.cx", "https://cpa.cryptomine.cx:8001"],
-//["cpa.optimusblue.com","https://cpa.optimusblue.com:8129"],
-//["cpa.youpool.io","http://cpa.youpool.io:8135"]
+[["poolparty.cryptopay.org.za",["https://partystats.cryptopay.org.za"],"CPA Pool Party","1"], 
+[["letshash.it",["https://letshash.it:8273"],"Letshash It","2"],
+[["cpacoin.my-mining-pool.de",["https://apicpacoin.my-mining-pool.de"],"DE Pool","3"]]
